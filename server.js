5433b3441b7509e4f35930119c473b58d4e12d93
Marius Herring <mariusherring@gmail.com>
2013-08-08 13:12:08 +0200

    Node project started

---
+++ server.js
@@ -0,0 +1,32 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mariusherring
+ * Date: 3/24/13
+ * Time: 7:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var http = require("http");
+var url = require("url");
+
+
+function start(route, handle) {
+    http.createServer(function (request, response) {
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.")
+
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
+    }).listen(8888);
+}
+
+exports.start = start;
+
