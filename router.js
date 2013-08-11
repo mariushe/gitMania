940d573a5666b14a1414ddbf50b53a5980d27f89
Marius Herring <mariusherring@gmail.com>
2013-08-11 17:40:16 +0200

    fixing js

--- router.js
+++ router.js
@@ -1,5 +1,7 @@
 function route(handle, pathname, response) {
+	
     console.log("About to route a request for " + pathname);
+
     if (typeof handle[pathname] === 'function') {
         handle[pathname](response);
     } else {
--- view/js/test.js
+++ view/js/test.js
@@ -1,3 +1,9 @@
-function addContent() {
-	document.getElementById("content").innerHTML = "Awsome!";
+function myFunction()
+{
+var x="",i;
+for (i=1; i<=6; i++)
+{
+x=x + "<h" + i + ">Heading " + i + "</h" + i + ">";
+}
+document.getElementById("demo").innerHTML=x;
 }
