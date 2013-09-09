9cfe116a6680e85d075fe2d6ffe9e99118889faa
Marius Herring <mariusherring@gmail.com>
2013-09-09 20:34:53 +0200

    More fun

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
 $("#myTable").tablesorter(); 
-	$("#submit").click(addNewResult);
+	$("#add").click(submitResult);
 
-	function addNewResult() {
+	function submitResult() {
 		var resultValue = $("#field").val();
+
+		if (resultValue === "") {
+			return;
+		}
+
 		$("#myTable").append("<tr><td>" + resultValue + "</td></tr>");
 		$("#field").val("");
 		$("#myTable").corner();
+	}
+
+	$("#submit").click(addNewRow);
+
+	function addNewRow() {
+		$("#notification").append("<div class='alert alert-success'><b>Well done!</b> You successfully pushed submit.</div>");
 	}
 });
