735a74ea06507514fc9fa74fd2d0a8defb0eb7d8
Marius Herring <mariusherring@gmail.com>
2013-09-09 20:13:03 +0200

    Changed table design from tablesorter to bootstrap

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ function start(route, handle) {
         route(handle, pathname, response);
     }
 
-    http.createServer(onRequest).listen(8888);
+    http.createServer(onRequest).listen(8080);
 
     console.log("Server has started.");
 }
