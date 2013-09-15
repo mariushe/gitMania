0bcc2b4f5c74d751afef148d7f9259c60e45b78c
Marius Herring <mariusherring@gmail.com>
2013-09-15 17:06:25 +0200

    Created git show JSON generator

--- gitShowHandler.js
+++ gitShowHandler.js
@@ -8,10 +8,59 @@ function generationShowJson(response, stdout) {
     var rawData = stdout.split('\n');
     var generated = {
         "hash" : rawData[0].substr(rawData[0].indexOf(' ') + 1),
-        "author" : rawData[1].substr(rawData[1].indexOf(' ') + 1)
+        "author" : rawData[1].substr(rawData[1].indexOf(' ') + 1),
+        "date" :  rawData[2].substr(rawData[2].indexOf(' ') + 1),
+        "msg" :  rawData[4].substr(rawData[4].indexOf(' ') + 1).trim(),
+        "diff" : []
     };
-
+    runThroughDifference(rawData, generated);
     gitResponse(response, generated);
+}
+
+function runThroughDifference(rawData, generated) {
+	for (var i = 5; i < rawData.length; i++) {
+		if (rawData[i].indexOf("diff") == 0) {
+			generated.diff.push({
+				"filename" : getFilename(rawData[i]),
+				"lines" : getChanges(i, rawData)
+			});
+
+		console.log(getFilename(rawData[i]));	
+
+		}
+		
+    }
+}
+
+function getChanges(index, rawData) {
+	for (var i = index; i < rawData.length; i++) {
+		if (rawData[i].indexOf("@@") == 0) {
+			return fetchLinesThatAreChanged(i+1, rawData);
+			console.log(getFilename(rawData[i]));	
+		}
+    }
+}
+
+function fetchLinesThatAreChanged(index, rawData) {
+
+	lines = [];
+
+	for (var i = index; i < rawData.length; i++) {
+
+		if (rawData[i].indexOf("diff") == 0 || rawData[i].indexOf("\\") == 0) {
+			return lines;
+		}
+
+		lines.push({"line" : rawData[i]});
+    }
+
+    return lines;
+}
+
+function getFilename(lineToParse) {
+	result = lineToParse.substr(lineToParse.indexOf("--git a/") + 8);
+	result = result.substr(0, result.indexOf(" b/"));
+	return result;
 }
 
 exports.generationShowJson = generationShowJson;
